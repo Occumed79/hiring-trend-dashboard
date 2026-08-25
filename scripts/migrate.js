16e5d2def7aadc9d35af1526f49c02a855dd357e
Occumed79 <Alex.Ayvazian@logan.edu>
2026-08-24 23:45:52 -0700

    Apply JobSpy runtime migration during builds

--- scripts/migrate.js
+++ scripts/migrate.js
@@ -24,6 +24,7 @@ async function migrate() {
     path.join(__dirname, '../db/hardening.sql'),
     path.join(__dirname, '../db/benchmark_hardening.sql'),
     path.join(__dirname, '../db/theirstack_runtime.sql'),
+    path.join(__dirname, '../db/jobspy_runtime.sql'),
     path.join(__dirname, '../db/source_seeds.sql'),
   ];
   const client = new Client({
